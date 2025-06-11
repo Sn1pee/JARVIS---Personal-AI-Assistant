b5d6682d6637303061531a07853bbfa3145f5c0f
Monish Rai <monishrai1801@gmail.com>
2025-06-11 17:37:13 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,10 +1,30 @@
-from google import genai
+import os
+from openai import OpenAI
 
-client = genai.Client(
-  api_key="YOUR_GEMINI_API_KEY"
+token = os.environ["GITHUB_TOKEN"]
+endpoint = "https://models.github.ai/inference"
+model = "openai/gpt-4.1"
+
+client = OpenAI(
+    base_url=endpoint,
+    api_key=token,
 )
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="What is coding?"
+response = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a virtual assistant named jarvis skilled in general tasks like Alexa and Google Cloud",
+        },
+        {
+            "role": "user",
+            "content": "What is Coding",
+        }
+    ],
+    temperature=1.0,
+    top_p=1.0,
+    model=model
 )
-print(response.text)
+
+print(response.choices[0].message.content)
+
